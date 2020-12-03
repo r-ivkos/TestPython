f086c1941b1518be20600910dad8cbff8e013063
r-ivkos <74710274+r-ivkos@users.noreply.github.com>
2020-12-03 21:09:13 +0100

    Update reqs.py

--- reqs.py
+++ reqs.py
@@ -1,22 +1,22 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+ADDRESS = "http://127.0.0.1:5000/"
 
 inp = input()
 while inp:
 	if inp == "GET":
-		response = requests.get(BASE+"tasks/")
+		response = requests.get(ADDRESS+"tasks/")
 		print(response.json())
 	elif inp == "POST":
 		inp = input("POST: ")
-		response = requests.post(BASE+"create_task/", {"name":inp})
+		response = requests.post(ADDRESS+"create_task/", {"name":inp})
 		print(response.json())
 	elif inp == "PUT":
 		inp = input("PUT: ")
-		response = requests.put(BASE+"task/"+inp)
+		response = requests.put(ADDRESS+"task/"+inp)
 		print(response.json())
 	elif inp == "PATCH":
 		inp = input("PATCH: ")
-		response = requests.patch(BASE+"task/"+inp)
+		response = requests.patch(ADDRESS+"task/"+inp)
 		print(response.json())
 	inp = input()
